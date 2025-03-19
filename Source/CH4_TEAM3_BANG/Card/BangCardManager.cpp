04a85475f2e6a385212fecb0282ef5444f3e8120
황찬호 <cksgh0316@gmail.com>
2025-03-19 16:34:25 +0900

    [Merge] To merge

--- Source/CH4_TEAM3_BANG/Card/BangCardManager.cpp
+++ Source/CH4_TEAM3_BANG/Card/BangCardManager.cpp
@@ -18,4 +18,33 @@ UBangCardBase* UBangCardManager::GetItemByName(const FText Name) const
 	return nullptr;
 }
 
+void GetAllCards()
+{
+	if (CardData)
+	{
+		for (UBangCardBase* Card : CardData->Cards)
+		{
+			if (Card && Card->CardName.EqualTo(Name))
+			{
+				return Card;
+			}
+		}
+	}
+	return nullptr;
+}
 
+// 덱 섞기
+void UBangCardManager::ShuffleDeck()
+{
+	if (AllCards.Num() == 0) return;
+
+	
+}
+
+// 카드 타입별 정리
+void UBangCardManager::CategorizeCardsByType()
+{
+	CardDeckByType.Empty();
+
+	
+}
--- Source/CH4_TEAM3_BANG/Card/BangCardManager.h
+++ Source/CH4_TEAM3_BANG/Card/BangCardManager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Data/CardEnums.h"
 #include "UObject/Object.h"
 #include "BangCardManager.generated.h"
 
@@ -13,10 +14,28 @@ class CH4_TEAM3_BANG_API UBangCardManager : public UObject
 	GENERATED_BODY()
 	
 public:
+	// DataAssect 접근용
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Manager")
 	UBangCardDataAsset* CardData;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Card Manager")
+	TArray<UBangCardDataAsset*> AllCards;
+	
+	TMap<ECardType, TArray<UBangCardDataAsset*>> CardDeckByType;
 
-	// 특정 이름의 카드을 찾는 함수
+	// 모든 카드 섞기
+	UFUNCTION(BlueprintCallable, Category = "Card Manager")
+	void ShuffleDeck();
+
+	// 카드 타입별 정리
+	UFUNCTION(BlueprintCallable, Category = "Card Manager")
+	void CategorizeCardsByType();
+
+	// 모든 카드 가져오기
+	UFUNCTION(BlueprintCallable, Category = "Card Manager")
+	void GetAllCards();
+
+	// 특정 이름의 카드을 찾는 함수 Sample
 	UFUNCTION(BlueprintCallable, Category = "Card Manager")
 	UBangCardBase* GetItemByName(FText Name) const;
 };
