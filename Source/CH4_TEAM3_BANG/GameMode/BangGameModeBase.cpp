e69ae243998e9c1a904fb22cdd00d59335f525b5
황찬호 <cksgh0316@gmail.com>
2025-03-20 17:33:04 +0900

    [Add] GameMode 로직 추가

--- Source/CH4_TEAM3_BANG/GameMode/BangGameModeBase.cpp
+++ Source/CH4_TEAM3_BANG/GameMode/BangGameModeBase.cpp
@@ -1,6 +1,40 @@
 #include "GameMode/BangGameModeBase.h"
 #include "PlayerController/BangPlayerController.h"
 
+/*
+*GetWorld()->GetTimerManager().SetTimer(
+        TimerHandle,
+        this,
+        &ABangGameMode::PrintPlayerStats,
+        10.0f,
+        true
+    );
+ *
+ * 
+ */
+/*
+void ABangGameModeBase::PrintPlayerStats()
+{
+    // GameState의 PlayerArray에서 모든 플레이어의 PlayerState 접근
+    if (AGameStateBase* GS = GetWorld()->GetGameState())
+    {
+        for (APlayerState* PlayerState : GS->PlayerArray)
+        {
+            if (ABangPlayerState* BangPlayerState = Cast<ABangPlayerState>(PlayerState))
+            {
+                BangPlayerState->PlayerStat.Value += 5;
+                UE_LOG(LogTemp, Error, TEXT("Player %s stat: %d"),
+                    *BangPlayerState->GetPlayerName(),
+                    BangPlayerState->PlayerStat.Value);
+                BangPlayerState->ForceNetUpdate();
+                // 강제 없뎃 말고
+                // 자연복제 주기 활용, 복제 설정 조정(NetUpdateFrequency) 또는 RPC로 직접 호출하여 적용해도 된다.
+            }
+        }
+    }
+}
+*/
+
 void ABangGameModeBase::StartGame_Implementation()
 {/*
     if (HasAuthority())
