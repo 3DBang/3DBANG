7e4ff14235dad9360b786301e60b36f6f9b64c42
황찬호 <cksgh0316@gmail.com>
2025-03-25 19:55:15 +0900

    [Update] To Merge

--- Source/CH4_TEAM3_BANG/PlayerController/BangPlayerController.cpp
+++ Source/CH4_TEAM3_BANG/PlayerController/BangPlayerController.cpp
@@ -53,6 +53,7 @@ void ABangPlayerController::BeginPlay()
 		GetWorld()->GetTimerManager().SetTimerForNextTick(this, &ABangPlayerController::GetPlayerStateAtBegin);
 	}
 
+	// 호스트는 직접 동작 수행 해줘야함
 	if (HasAuthority())
 	{
 		TryBindPlayerInfoUpdated();
