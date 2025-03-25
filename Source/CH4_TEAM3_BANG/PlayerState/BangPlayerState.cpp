4934d345fa17d0bf4f3a22bf9c89987388b05547
황찬호 <cksgh0316@gmail.com>
2025-03-25 21:16:06 +0900

    [Add] PS 기능추가

--- Source/CH4_TEAM3_BANG/PlayerState/BangPlayerState.cpp
+++ Source/CH4_TEAM3_BANG/PlayerState/BangPlayerState.cpp
@@ -392,6 +392,27 @@ bool ABangPlayerState::CheckIsCardAble(const int32 FromUniqueID, const FSingleCa
 	return true;
 }
 
+bool ABangPlayerState::CheckIsCardAbleByPassive(const int32 FromUniqueID, const EPassiveType PassiveType)
+{
+	if (!CardManager) return false;
+
+	for (auto [SymbolType, SymbolNumber] : PlayerInfo.GetPlayerInformation(FromUniqueID)->EquippedCards.PlayerCards)
+	{
+		FSingleCard OutFoundCard;
+		CardManager->GetCardBySymbolAndNumberFromDataAsset(SymbolType, SymbolNumber, OutFoundCard);
+
+		if (const TObjectPtr<UBangPassiveCard> BangPassiveCard = Cast<UBangPassiveCard>(OutFoundCard.Card))
+		{
+			if (PassiveType == BangPassiveCard->PassiveType)
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
 void ABangPlayerState::Server_CheckCardSymbol_Implementation(const uint32& FromUniqueID, const uint16& CardCount)
 {
 	const TObjectPtr<ABangGameMode> GameMode = GetWorld()->GetAuthGameMode<ABangGameMode>();
